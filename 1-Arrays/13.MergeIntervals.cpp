1d862ad4ff0e53428011c9f4b7428e4fb67e0a8a
Aaditya Patil <131251920+Aaditatgithub@users.noreply.github.com>
2023-08-13 11:53:23 +0530

    Create 13.MergeIntervals.cpp

---
+++ 1-Arrays/13.MergeIntervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        int n = intervals.size();                //number of intervals given
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> output;
+        
+        for(vector<int> interval : intervals){
+            if(output.empty() || output.back()[1] < interval[0]){
+                output.push_back(interval);
+            }
+            else{
+                output.back()[1] = max(output.back()[1], interval[1]);
+            }
+        }
+        return output;
+    }
+};
