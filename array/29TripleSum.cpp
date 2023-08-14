10ecbaac6e9d38b9e7d70198f34c0f3aa8015846
Adi-vig <aditya2004cs@gmail.com>
2023-08-14 22:24:52 +0530

    .

---
+++ array/29TripleSum.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+    {
+        //Your Code Here
+        // problem is next variation 
+        
+        // save array element and index in map to reduce search complexity 
+        
+        unordered_map<int, int>se;
+        
+        for(int i=0 ; i<n ; i++ ){
+            se[A[i]] = i ;
+        }
+        
+        
+        for(int i=0; i< n; i++){
+            for(int j= i+1 ; j<n ; j++){
+                
+                if(se[X - (A[i]+A[j])] > j   )return true;
+                
+            }
+        }
+        
+        return false;
+        
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n,X;
+		cin>>n>>X;
+		int i,A[n];
+		for(i=0;i<n;i++)
+			cin>>A[i];
+		Solution ob;
+        cout <<  ob.find3Numbers(A, n, X) << endl;
+    }
+}
+
+// } Driver Code Ends
