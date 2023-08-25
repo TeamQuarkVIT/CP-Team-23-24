a40c6c558629dd4813b49539690fbee883e423db
Sailee Dorle <139053879+sailee786@users.noreply.github.com>
2023-08-25 19:27:51 +0530

    Add files via upload

---
+++ strings/Codeforces Checking.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        char n;
+        cin>>n;
+        if(n=='c'||n=='o'||n=='d'||n=='e'||n=='f'||n=='r'||n=='s')
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
