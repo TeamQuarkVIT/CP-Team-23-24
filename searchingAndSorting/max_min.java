6d132583775a1d81d01d78ba5197542dc2f170cc
onkarbhojane <122461938+onkarbhojane@users.noreply.github.com>
2023-09-14 16:27:33 +0530

    Create max_min.java

---
+++ searchingAndSorting/max_min.java
@@ -0,0 +1,18 @@
+class Solution
+{ 
+    public static int findSum(int A[],int N) 
+    {
+        //code here
+        int max=A[0];
+        int min=A[0];
+        for (int i=0;i<N;i++){
+            if(max<A[i]){
+                max=A[i];
+            }
+            if(min>A[i]){
+                min=A[i];
+            }
+        }
+        return max+min;
+    }
+}
