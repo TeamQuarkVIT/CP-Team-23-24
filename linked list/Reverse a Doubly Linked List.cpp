be46a6614c406fb6c690b1fde74ed2d639c30fa7
Sailee Dorle <139053879+sailee786@users.noreply.github.com>
2023-09-18 10:56:31 +0530

    Add files via upload

---
+++ linked list/Reverse a Doubly Linked List.cpp
@@ -0,0 +1,16 @@
+Node* reverseDLL(Node * head)
+{
+    Node* temp = NULL;
+    Node* curr = head;
+ 
+    while (curr != NULL) {
+        temp = curr->prev;
+        curr->prev = curr->next;
+        curr->next = temp;
+        curr = curr->prev;
+    }
+
+    if (temp != NULL)
+    head = temp->prev;
+    return head;
+}
